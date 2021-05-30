496009cc0684465e8c8da4796355114f15d0f52d
MaxStouten <maxstouten@hotmail.com>
2021-05-30 21:04:02 +0200

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**Optionally add Bootstrap to your project**/
+// import'tailwindcss';
+import '../css/style.scss';
+
+
+/********** Paste your code here! ************/
+
+console.log('Paste your code here!');
+var password;
+
+var pass1="dyson";
+
+password=prompt('Wat is het prosit wachtwoord?',' ');
+
+
